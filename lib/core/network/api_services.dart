956fcdc183ecdc22a302ad63dfda3ceaad73f301
qassem abied <qassemab1@gmail.com>
2026-01-23 00:54:57 +0200

    add add popular, now playing, top rated and upcoming endpoints

--- lib/core/network/api_services.dart
+++ lib/core/network/api_services.dart
@@ -2,12 +2,34 @@ import 'package:dio/dio.dart';
 import 'package:retrofit/error_logger.dart';
 import 'package:retrofit/http.dart';
 import 'package:untitled3/core/network/api_constance.dart';
+import 'package:untitled3/core/network/api_end_points.dart';
+
+import '../../features/home/data/models/movies_response_models.dart';
 
 part 'api_services.g.dart';
 
-@RestApi(baseUrl:ApiConstance.BaseUrl )
+@RestApi(baseUrl: ApiConstance.BaseUrl)
 abstract class ApiServices {
- factory ApiServices(Dio dio ,{String baseUrl})= _ApiServices;
+  factory ApiServices(Dio dio, {String baseUrl}) = _ApiServices;
 
-
+  @GET(ApiEndPoints.popularApi)
+  Future<MoviesResponse> getPopularMovies({
+    @Query('page') required int page,
+    @Query('language') required String language,
+  });
+  @GET(ApiEndPoints.playingNow)
+  Future<MoviesResponse> getPlayingNowMovies({
+    @Query('page') required int page,
+    @Query('language') required String language,
+  });
+  @GET(ApiEndPoints.topRated)
+  Future<MoviesResponse> getTopRatedMovies({
+    @Query('page') required int page,
+    @Query('language') required String language,
+  });
+  @GET(ApiEndPoints.upComing)
+  Future<MoviesResponse> getUpComingMovies({
+    @Query('page') required int page,
+    @Query('language') required String language,
+  });
 }
