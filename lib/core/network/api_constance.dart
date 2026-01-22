c5ee86de35147e8b07612814bea0d1fc3dd62230
qassem abied <qassemab1@gmail.com>
2026-01-23 00:52:27 +0200

    refactor api constance

--- lib/core/network/api_constance.dart
+++ lib/core/network/api_constance.dart
@@ -1,8 +1,13 @@
-class ApiEndPoint {
+class ApiConstance {
   static const String BaseUrl = 'https://api.themoviedb.org/3';
   static const String BearerToken='eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5YjVhMGE1YzgxZTkxOWJhNmM3ZTdlM2ZhYWJjZmQwZiIsIm5iZiI6MTY2NjY1NzA0NC4xNTgwMDAyLCJzdWIiOiI2MzU3MmIxNDVmNGI3MzAwN2I4NmM1YjAiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.CjAcAfkFU2HV-et8T3omUz-NoRCPV54hiaDFBk564JU';
   static const String ApiKey = '9b5a0a5c81e919ba6c7e7e3faabcfd0f';
-  static String imagePath(String path) =>
-      'https://image.tmdb.org/t/p/w500$path';
-
+  static const String imageBaseUrl =
+      'https://image.tmdb.org/t/p/w500';
+  static String imagePath(String? path) {
+    if (path == null || path.isEmpty) {
+      return 'https://via.placeholder.com/500x281?text=No+Image';
+    }
+    return '$imageBaseUrl$path';
+  }
 }
