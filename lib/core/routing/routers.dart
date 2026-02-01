a748eaa41ca7e81279ac1e20e4f867a364744f7b
qassem abied <qassemab1@gmail.com>
2026-02-01 02:29:31 +0200

    add botton nav view

--- lib/core/routing/routers.dart
+++ lib/core/routing/routers.dart
@@ -1,4 +1,6 @@
 class Routes{
   static const String homeScreen='/Home';
   static const String allMoviesScreen='/AllMovies';
+  static const String detailsMoviesScreen='/detailsMovies';
+  static const String bottonNavScreen='/bottonNav';
 }
