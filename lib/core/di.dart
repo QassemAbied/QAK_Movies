8491baf6fe331e2424822e1e554a0520779eadb0
qassem abied <qassemab1@gmail.com>
2026-02-04 00:20:12 +0200

    to update flutter version

--- lib/core/di.dart
+++ lib/core/di.dart
@@ -1,119 +1,133 @@
+import 'package:dio/dio.dart';
 import 'package:get_it/get_it.dart';
-import 'package:untitled3/data/data_sources/authentication_remote_data_source.dart';
-import 'package:untitled3/data/data_sources/base_remote_data_sources.dart';
-import 'package:untitled3/data/data_sources/favorite_remote_data_source.dart';
-import 'package:untitled3/data/data_sources/remote_data_source_impl.dart';
-import 'package:untitled3/data/data_sources/watchlist_remote_data_source.dart';
-import 'package:untitled3/data/repository_data/authentication_repository.dart';
-import 'package:untitled3/data/repository_data/favorite_repository_impl.dart';
-import 'package:untitled3/data/repository_data/repository_impl.dart';
-import 'package:untitled3/data/repository_data/watchlist_repository.dart';
-import 'package:untitled3/domain/repository_domain/authentication.dart';
-import 'package:untitled3/domain/repository_domain/base_repository.dart';
-import 'package:untitled3/domain/repository_domain/favorite_repository.dart';
-import 'package:untitled3/domain/repository_domain/watchlist_repository.dart';
-import 'package:untitled3/domain/use_case/authentication_use_case/create_session_use_case.dart';
-import 'package:untitled3/domain/use_case/authentication_use_case/login_use_case.dart';
-import 'package:untitled3/domain/use_case/authentication_use_case/request_token_use_case.dart';
-import 'package:untitled3/domain/use_case/details_use_case/castmovie_usecase.dart';
-import 'package:untitled3/domain/use_case/details_use_case/check_account_usecase.dart';
-import 'package:untitled3/domain/use_case/details_use_case/detail_usecase.dart';
-import 'package:untitled3/domain/use_case/favorite_use_case/favorite_usecase.dart';
-import 'package:untitled3/domain/use_case/favorite_use_case/remove_favorite_use_case.dart';
-import 'package:untitled3/domain/use_case/movies_use_case/genre_usecase.dart';
-import 'package:untitled3/domain/use_case/details_use_case/imagemovie_uescase.dart';
-import 'package:untitled3/domain/use_case/movies_use_case/moviebygenre_usecase.dart';
-import 'package:untitled3/domain/use_case/movies_use_case/popular_usecase.dart';
-import 'package:untitled3/domain/use_case/search_usecase.dart';
-import 'package:untitled3/domain/use_case/details_use_case/similar_usecase.dart';
-import 'package:untitled3/domain/use_case/movies_use_case/toprated_usecase.dart';
-import 'package:untitled3/domain/use_case/movies_use_case/trend_usecase.dart';
-import 'package:untitled3/domain/use_case/movies_use_case/upcoming_usecase.dart';
-import 'package:untitled3/domain/use_case/details_use_case/video_use_case.dart';
-import 'package:untitled3/domain/use_case/watchlist_use_case/add_watchlist_usecase.dart';
-import 'package:untitled3/domain/use_case/watchlist_use_case/remove_watchlist_usecase.dart';
-import 'package:untitled3/domain/use_case/watchlist_use_case/watchlist_usecase.dart';
-import 'package:untitled3/presentation/state_management/authentication_bloc/authentication_bloc.dart';
-import 'package:untitled3/presentation/state_management/cubit/cubit.dart';
-import 'package:untitled3/presentation/state_management/favorite_bloc/favorit_bloc.dart';
-import 'package:untitled3/presentation/state_management/movie_bloc/movie_bloc.dart';
-import 'package:untitled3/presentation/state_management/movie_details_bloc/details_bloc.dart';
-import 'package:untitled3/presentation/state_management/search_bloc/search_bloc.dart';
+import 'package:untitled3/core/theming/theme_cubit/app_theme_cubit.dart';
+import 'package:untitled3/core/theming/theme_eunm/them_eunm.dart';
 
-import '../domain/use_case/favorite_use_case/add_favorite_use_case.dart';
-import '../presentation/state_management/watchlist_bloc/watchlist_bloc.dart';
+import 'package:untitled3/features/all_movies/data/repos/all_movies_repos.dart';
+import 'package:untitled3/features/all_movies/data/repos/all_movies_repos_impl.dart';
+import 'package:untitled3/features/details_movies/controller/details_movies_cubit.dart';
+import 'package:untitled3/features/details_movies/data/repos/details_movies_repos.dart';
+import 'package:untitled3/features/details_movies/data/repos/details_movies_repos_impl.dart';
+import 'package:untitled3/features/favorites/data/repo/favorites_repos.dart';
+import 'package:untitled3/features/favorites/data/repo/favorites_repos_impl.dart';
+import 'package:untitled3/features/home/controller/movies_cubit.dart';
+import 'package:untitled3/features/search/controller/search_cubit.dart';
+import 'package:untitled3/features/search/data/repos/search_repos.dart';
+import 'package:untitled3/features/search/data/repos/search_repos_impl.dart';
+import 'package:untitled3/features/watch_list/controller/watch_list_cubit.dart';
+import 'package:untitled3/features/watch_list/data/repos/watch_list_repos.dart';
+import 'package:untitled3/features/watch_list/data/repos/watch_list_repos_implement.dart';
+
+import '../features/favorites/controller/favorites_cubit.dart';
+import '../features/home/controller/genres_cubit.dart';
+import '../features/home/data/repos/movies_repos.dart';
+import '../features/home/data/repos/movies_repos_impl.dart';
+import 'network/api_services.dart';
+import 'network/dio_factory.dart';
 
 final Sl = GetIt.instance;
 
 class ServiceLocator {
   void init() {
-    Sl.registerLazySingleton<RemoteDataSources>(() => RemoteDataSourceImpl());
-    Sl.registerLazySingleton<BaseRepository>(() => RepositoryImpl(Sl()));
-    Sl.registerLazySingleton<getPopularUseCase>(() => getPopularUseCase(Sl()));
-    Sl.registerLazySingleton<getGenreUseCase>(() => getGenreUseCase(Sl()));
-    Sl.registerLazySingleton<getMovieByGenresUseCase>(
-        () => getMovieByGenresUseCase(Sl()));
-    Sl.registerLazySingleton<getTopRatedUseCase>(
-        () => getTopRatedUseCase(Sl()));
-    Sl.registerLazySingleton<getUpComingUseCase>(
-        () => getUpComingUseCase(Sl()));
-    Sl.registerLazySingleton<getTrendUseCase>(() => getTrendUseCase(Sl()));
-    Sl.registerLazySingleton<getDetailsMovieUseCase>(
-        () => getDetailsMovieUseCase(Sl()));
-    Sl.registerLazySingleton<getVideoMovieUseCase>(
-        () => getVideoMovieUseCase(Sl()));
-    Sl.registerLazySingleton<getImageMovieUseCase>(
-        () => getImageMovieUseCase(Sl()));
-    Sl.registerLazySingleton<getCastMovieUseCase>(
-        () => getCastMovieUseCase(Sl()));
-    Sl.registerLazySingleton<getSimilarMovieUseCase>(
-        () => getSimilarMovieUseCase(Sl()));
-    Sl.registerLazySingleton<getSearchMovieUseCase>(
-        () => getSearchMovieUseCase(Sl()));
-    Sl.registerLazySingleton<getCheckAccountStatesUseCase>(
-        () => getCheckAccountStatesUseCase(Sl()));
-    Sl.registerLazySingleton<WatchListRepository>(
-        () => WatchListRepositoryImpl(Sl()));
-    Sl.registerLazySingleton<WatchlistLocalDataSource>(
-        () => WatchlistLocalDataSourceImpl());
-    Sl.registerLazySingleton<getWatchListUseCase>(
-        () => getWatchListUseCase(Sl()));
-    Sl.registerLazySingleton<getAddWatchListUseCase>(
-        () => getAddWatchListUseCase(Sl()));
-    Sl.registerLazySingleton<getRemoveWatchListUseCase>(
-        () => getRemoveWatchListUseCase(Sl()));
+
+    Dio dio = DioFactory.getDio();
+    Sl.registerLazySingleton<ApiServices>(() => ApiServices(dio));
 
 
-    Sl.registerLazySingleton<FavoriteRepository>(
-        () => FavoriteRepositoryImpl(Sl()));
-    Sl.registerLazySingleton<FavoriteRemoteDataSource>(
-        () => FavoriteRemoteDataSourceImpl());
-    Sl.registerLazySingleton<FavoriteUseCase>(() => FavoriteUseCase(Sl()));
-    Sl.registerLazySingleton<getAddFavoriteUseCase>(
-        () => getAddFavoriteUseCase(Sl()));
-    Sl.registerLazySingleton<getRemoveFavoriteUseCase>(
-        () => getRemoveFavoriteUseCase(Sl()));
+    Sl.registerLazySingleton<MoviesRepository>(
+          () => MoviesRepositoryImpl(Sl<ApiServices>()),
 
-    Sl.registerLazySingleton<AuthenticationRepository>(
-        () => AuthenticationRepositoryImpl(Sl()));
-    Sl.registerLazySingleton<AuthenticationRemoteDataSource>(
-        () => AuthenticationRemoteDataSourceImpl());
-    Sl.registerLazySingleton<RequestTokenUseCase>(
-        () => RequestTokenUseCase(Sl()));
-    Sl.registerLazySingleton<getLoginValidateUseCase>(
-        () => getLoginValidateUseCase(Sl()));
-    Sl.registerLazySingleton<getCreateSessionUseCase>(
-        () => getCreateSessionUseCase(Sl()));
+    );
+    Sl.registerLazySingleton<AllMoviesRepository>(
+            () => AllMoviesRepositoryImpl(Sl<ApiServices>()),);
+    Sl.registerFactory(() => HomeCubit(Sl()));
+      Sl.registerFactory(() => GenreCubit(Sl()));
+    Sl.registerFactory(() => AppThemeCubit( AppThemeMode.system,
+      AppLanguage.english,));
 
-    Sl.registerFactory(() => MovieBloc(Sl(), Sl(), Sl(), Sl(), Sl(), Sl()));
-    Sl.registerFactory(
-        () => MovieDetailsBloc(Sl(), Sl(), Sl(), Sl(), Sl(), Sl()));
+    Sl.registerLazySingleton<DetailsMoviesRepository>(
+          () => DetailsMoviesRepositoryImpl(Sl<ApiServices>()),);
+    Sl.registerFactory(() => DetailsMoviesCubit(Sl()));
 
 
-    Sl.registerFactory(() => ChangeBottomCubit());
-    Sl.registerFactory(() => SearchMovieBloc(Sl()));
-    Sl.registerFactory(() => WatchlistBloc(Sl(), Sl(), Sl()));
-    Sl.registerFactory(() => FavoritesBloc(Sl(), Sl(), Sl()));
-    Sl.registerFactory(() => AuthenticationBloc(Sl(), Sl(), Sl()));
+    Sl.registerLazySingleton<FavoritesRepos>(
+          () => FavoritesReposImpl(Sl<ApiServices>()),);
+    Sl.registerFactory(() => FavoritesCubit(Sl()));
+
+    Sl.registerLazySingleton<WatchListRepos>(
+          () => WatchListReposImplement(Sl<ApiServices>()),);
+    Sl.registerFactory(() => WatchListCubit(Sl()));
+
+    Sl.registerLazySingleton<SearchRepos>(
+          () => SearchReposImpl(Sl<ApiServices>()),);
+   // Sl.registerFactory(() => SearchCubit(Sl(), ));
+    //Sl.registerLazySingleton<RemoteDataSources>(() => RemoteDataSourceImpl());
+    // Sl.registerLazySingleton<BaseRepository>(() => RepositoryImpl(Sl()));
+    // Sl.registerLazySingleton<getPopularUseCase>(() => getPopularUseCase(Sl()));
+    // Sl.registerLazySingleton<getGenreUseCase>(() => getGenreUseCase(Sl()));
+    // Sl.registerLazySingleton<getMovieByGenresUseCase>(
+    //     () => getMovieByGenresUseCase(Sl()));
+    // Sl.registerLazySingleton<getTopRatedUseCase>(
+    //     () => getTopRatedUseCase(Sl()));
+    // Sl.registerLazySingleton<getUpComingUseCase>(
+    //     () => getUpComingUseCase(Sl()));
+    // Sl.registerLazySingleton<getTrendUseCase>(() => getTrendUseCase(Sl()));
+    // Sl.registerLazySingleton<getDetailsMovieUseCase>(
+    //     () => getDetailsMovieUseCase(Sl()));
+    // Sl.registerLazySingleton<getVideoMovieUseCase>(
+    //     () => getVideoMovieUseCase(Sl()));
+    // Sl.registerLazySingleton<getImageMovieUseCase>(
+    //     () => getImageMovieUseCase(Sl()));
+    // Sl.registerLazySingleton<getCastMovieUseCase>(
+    //     () => getCastMovieUseCase(Sl()));
+    // Sl.registerLazySingleton<getSimilarMovieUseCase>(
+    //     () => getSimilarMovieUseCase(Sl()));
+    // Sl.registerLazySingleton<getSearchMovieUseCase>(
+    //     () => getSearchMovieUseCase(Sl()));
+    // Sl.registerLazySingleton<getCheckAccountStatesUseCase>(
+    //     () => getCheckAccountStatesUseCase(Sl()));
+    // Sl.registerLazySingleton<WatchListRepository>(
+    //     () => WatchListRepositoryImpl(Sl()));
+    // Sl.registerLazySingleton<WatchlistLocalDataSource>(
+    //     () => WatchlistLocalDataSourceImpl());
+    // Sl.registerLazySingleton<getWatchListUseCase>(
+    //     () => getWatchListUseCase(Sl()));
+    // Sl.registerLazySingleton<getAddWatchListUseCase>(
+    //     () => getAddWatchListUseCase(Sl()));
+    // Sl.registerLazySingleton<getRemoveWatchListUseCase>(
+    //     () => getRemoveWatchListUseCase(Sl()));
+    //
+    //
+    // Sl.registerLazySingleton<FavoriteRepository>(
+    //     () => FavoriteRepositoryImpl(Sl()));
+    // Sl.registerLazySingleton<FavoriteRemoteDataSource>(
+    //     () => FavoriteRemoteDataSourceImpl());
+    // Sl.registerLazySingleton<FavoriteUseCase>(() => FavoriteUseCase(Sl()));
+    // Sl.registerLazySingleton<getAddFavoriteUseCase>(
+    //     () => getAddFavoriteUseCase(Sl()));
+    // Sl.registerLazySingleton<getRemoveFavoriteUseCase>(
+    //     () => getRemoveFavoriteUseCase(Sl()));
+    //
+    // Sl.registerLazySingleton<AuthenticationRepository>(
+    //     () => AuthenticationRepositoryImpl(Sl()));
+    // Sl.registerLazySingleton<AuthenticationRemoteDataSource>(
+    //     () => AuthenticationRemoteDataSourceImpl());
+    // Sl.registerLazySingleton<RequestTokenUseCase>(
+    //     () => RequestTokenUseCase(Sl()));
+    // Sl.registerLazySingleton<getLoginValidateUseCase>(
+    //     () => getLoginValidateUseCase(Sl()));
+    // Sl.registerLazySingleton<getCreateSessionUseCase>(
+    //     () => getCreateSessionUseCase(Sl()));
+
+    // Sl.registerFactory(() => MovieBloc(Sl(), Sl(), Sl(), Sl(), Sl(), Sl()));
+    // Sl.registerFactory(
+    //     () => MovieDetailsBloc(Sl(), Sl(), Sl(), Sl(), Sl(), Sl()));
+    //
+    //
+    // Sl.registerFactory(() => ChangeBottomCubit());
+    // Sl.registerFactory(() => SearchMovieBloc(Sl()));
+    // Sl.registerFactory(() => WatchlistBloc(Sl(), Sl(), Sl()));
+    // Sl.registerFactory(() => FavoritesBloc(Sl(), Sl(), Sl()));
+    // Sl.registerFactory(() => AuthenticationBloc(Sl(), Sl(), Sl()));
   }
 }
--- lib/features/details_movies/data/models/credits_response.g.dart
+++ lib/features/details_movies/data/models/credits_response.g.dart
@@ -12,17 +12,10 @@ CreditsResponse _$CreditsResponseFromJson(Map<String, dynamic> json) =>
       cast: (json['cast'] as List<dynamic>?)
           ?.map((e) => Cast.fromJson(e as Map<String, dynamic>))
           .toList(),
-      crew: (json['crew'] as List<dynamic>?)
-          ?.map((e) => Crew.fromJson(e as Map<String, dynamic>))
-          .toList(),
     );
 
 Map<String, dynamic> _$CreditsResponseToJson(CreditsResponse instance) =>
-    <String, dynamic>{
-      'id': instance.id,
-      'cast': instance.cast,
-      'crew': instance.crew,
-    };
+    <String, dynamic>{'id': instance.id, 'cast': instance.cast};
 
 Cast _$CastFromJson(Map<String, dynamic> json) => Cast(
   adult: json['adult'] as bool?,
@@ -52,32 +45,4 @@ Map<String, dynamic> _$CastToJson(Cast instance) => <String, dynamic>{
   'character': instance.character,
   'credit_id': instance.creditId,
   'order': instance.order,
-};
-
-Crew _$CrewFromJson(Map<String, dynamic> json) => Crew(
-  adult: json['adult'] as bool?,
-  gender: (json['gender'] as num?)?.toInt(),
-  id: (json['id'] as num?)?.toInt(),
-  knownForDepartment: json['known_for_department'] as String?,
-  name: json['name'] as String?,
-  originalName: json['original_name'] as String?,
-  popularity: (json['popularity'] as num?)?.toDouble(),
-  profilePath: json['profile_path'] as String?,
-  creditId: json['credit_id'] as String?,
-  department: json['department'] as String?,
-  job: json['job'] as String?,
-);
-
-Map<String, dynamic> _$CrewToJson(Crew instance) => <String, dynamic>{
-  'adult': instance.adult,
-  'gender': instance.gender,
-  'id': instance.id,
-  'known_for_department': instance.knownForDepartment,
-  'name': instance.name,
-  'original_name': instance.originalName,
-  'popularity': instance.popularity,
-  'profile_path': instance.profilePath,
-  'credit_id': instance.creditId,
-  'department': instance.department,
-  'job': instance.job,
 };
