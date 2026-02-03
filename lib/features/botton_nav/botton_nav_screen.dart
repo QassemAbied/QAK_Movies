e086c505d79fc46b186596efe13b3e3b8df1566a
qassem abied <qassemab1@gmail.com>
2026-02-03 23:54:44 +0200

    update botton nav bar

--- lib/features/botton_nav/botton_nav_screen.dart
+++ lib/features/botton_nav/botton_nav_screen.dart
@@ -2,10 +2,11 @@ import 'package:conditional_builder_null_safety/conditional_builder_null_safety.
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:untitled3/core/helpers/extension.dart';
-import 'package:untitled3/features/search_screen.dart';
-import 'package:untitled3/features/watch_kist_secreen.dart';
+import 'package:untitled3/features/search/view/search_screen.dart';
+import '../../generated/l10n.dart';
 import '../favorites/view/favorite_screen.dart';
 import '../home/view/home_screen.dart';
+import '../profile/view/settings_screen.dart';
 import 'controller/botton_nav_cubit.dart';
 import 'controller/botton_nav_state.dart';
 
@@ -15,8 +16,7 @@ class BottonNavScreen extends StatelessWidget {
     const HomeScreen(),
     const SearchScreen(),
      const FavoriteScreen(),
-
-    const WatchKistSecreen(),
+    const SettingsScreen(),
   ];
   @override
   Widget build(BuildContext context) {
@@ -33,29 +33,29 @@ class BottonNavScreen extends StatelessWidget {
             },
           ),
           bottomNavigationBar: BottomNavigationBar(
-            fixedColor: context.colors.onSurface,
+            fixedColor: context.colors.primary,
             elevation: 0.0,
             currentIndex: ChangeBottomCubit.get(context).currentIndex,
             onTap: (index) {
               ChangeBottomCubit.get(context).changeBottom(index);
             },
             type: BottomNavigationBarType.fixed,
-            items: const [
+            items: [
               BottomNavigationBarItem(
-                icon: Icon(Icons.home_outlined),
-                label: 'Home',
+                icon: const Icon(Icons.home_outlined),
+                label: S.of(context).Home,
               ),
-              BottomNavigationBarItem(
-                icon: Icon(Icons.search),
-                label: 'Search',
+               BottomNavigationBarItem(
+                icon: const Icon(Icons.search),
+                label: S.of(context).Search,
               ),
-              BottomNavigationBarItem(
-                icon: Icon(Icons.favorite),
-                label: 'Favorite',
+               BottomNavigationBarItem(
+                icon: const Icon(Icons.favorite),
+                label: S.of(context).Favorite,
               ),
-              BottomNavigationBarItem(
-                icon: Icon(Icons.bookmark),
-                label: 'WatchList',
+               BottomNavigationBarItem(
+                icon: const Icon(Icons.settings),
+                label:S.of(context).settings,
               ),
             ],
           ),
