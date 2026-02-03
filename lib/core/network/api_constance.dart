a6ba52ca4a60102da128c52715c30d264bfe7043
qassem abied <qassemab1@gmail.com>
2026-02-03 23:44:44 +0200

    add new constance

--- lib/core/network/api_constance.dart
+++ lib/core/network/api_constance.dart
@@ -1,6 +1,6 @@
 class ApiConstance {
   static const String BaseUrl = 'https://api.themoviedb.org/3';
-  static const String BearerToken='eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5YjVhMGE1YzgxZTkxOWJhNmM3ZTdlM2ZhYWJjZmQwZiIsIm5iZiI6MTY2NjY1NzA0NC4xNTgwMDAyLCJzdWIiOiI2MzU3MmIxNDVmNGI3MzAwN2I4NmM1YjAiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.CjAcAfkFU2HV-et8T3omUz-NoRCPV54hiaDFBk564JU';
+  static const String BearerToken='eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4MzM5OTkyYWY5YWNiMzY5MTljZWQzNDEzYmZjZjJhZiIsIm5iZiI6MTY2NjY1NzA0NC4xNTgwMDAyLCJzdWIiOiI2MzU3MmIxNDVmNGI3MzAwN2I4NmM1YjAiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.6z8pD3j7K68GOXDk4cDevgdf5LRWyaaQyg9xCXmB2B8';
   static const String ApiKey = '9b5a0a5c81e919ba6c7e7e3faabcfd0f';
   static const String imageBaseUrl =
       'https://image.tmdb.org/t/p/w500';
