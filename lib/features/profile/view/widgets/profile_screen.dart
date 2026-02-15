ffb24cf6f46ec17b49eb923b4eb628777a3726be
qassem abied <qassemab1@gmail.com>
2026-02-15 02:52:12 +0200

    refactor screen

--- lib/features/profile/view/widgets/profile_screen.dart
+++ lib/features/profile/view/widgets/profile_screen.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:untitled3/core/helpers/spacing.dart';
-
-import '../../controller/profile_riverpod.dart';
+import '../../../providers.dart';
 
 String getCountryName(String code) {
   switch (code) {
