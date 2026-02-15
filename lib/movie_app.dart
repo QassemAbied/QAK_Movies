5e2f1334325cb633679edf3d6fb93b8ed4477c87
qassem abied <qassemab1@gmail.com>
2026-02-15 02:43:37 +0200

    Builds MaterialApp based on current app settings (theme & locale),using Riverpod for reactive global configuration.

--- lib/movie_app.dart
+++ lib/movie_app.dart
@@ -1,16 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
-import 'package:untitled3/features/watch_list/controller/watch_list_cubit.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'core/helpers/extension.dart';
 import 'core/routing/router_app.dart';
 import 'core/routing/routers.dart';
 import 'core/theming/app_theme.dart';
-import 'core/theming/theme_cubit/app_theme_cubit.dart';
-import 'core/theming/theme_cubit/app_theme_state.dart';
-import 'features/favorites/controller/favorites_cubit.dart';
-import 'features/home/controller/genres_cubit.dart';
-import 'features/home/controller/movies_cubit.dart';
+import 'core/theming/settings_controller/settings_riverpod.dart';
 import 'generated/l10n.dart';
 
 class MovieApp extends StatelessWidget {
@@ -18,38 +13,29 @@ class MovieApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  BlocListener<AppThemeCubit, AppThemeState>(
-      listener: (context, state) {
-        if (state is AppChangeLanguageState) {
-          final lang = state.language.code;
+    return Consumer(
 
-          context.read<HomeCubit>().loadHome(lang);
-          context.read<GenreCubit>().loadGenresMovies(lang);
-          context.read<FavoritesCubit>().loadFavoritesMovies(language: lang);
-          context.read<WatchListCubit>().loadWatchList(language: lang);
-        }
-      },
-      child: BlocBuilder<AppThemeCubit, AppThemeState>(
-        builder: (context, state) {
-          return MaterialApp(
-            locale: AppThemeCubit.get(context).localeLanguage(),
-            localizationsDelegates: const [
-              S.delegate,
-              GlobalMaterialLocalizations.delegate,
-              GlobalWidgetsLocalizations.delegate,
-              GlobalCupertinoLocalizations.delegate,
-            ],
-            supportedLocales: S.delegate.supportedLocales,
-            debugShowCheckedModeBanner: false,
-            title: 'Flutter Demo',
-            theme: AppTheme.lightTheme(),
-            darkTheme: AppTheme.darkTheme(),
-            themeMode: AppThemeCubit.get(context).themeMode(),
-            initialRoute: Routes.bottonNavScreen,
-            onGenerateRoute: RouterApp.generateRoute,
-          );
-        },
-      ),
+      builder: (BuildContext context, WidgetRef ref, Widget? child) {
+        final settings = ref.watch(appSettingsProvider);
+        return MaterialApp(
+           locale: settings.locale.toLocale(),
+          localizationsDelegates: const [
+            S.delegate,
+            GlobalMaterialLocalizations.delegate,
+            GlobalWidgetsLocalizations.delegate,
+            GlobalCupertinoLocalizations.delegate,
+          ],
+          supportedLocales: S.delegate.supportedLocales,
+          debugShowCheckedModeBanner: false,
+          title: 'Flutter Demo',
+          theme: AppTheme.lightTheme(),
+          darkTheme: AppTheme.darkTheme(),
+          themeMode: settings.themeMode.toThemeMode(),
+          initialRoute: Routes.bottonNavScreen,
+          onGenerateRoute: RouterApp.generateRoute,
+        );
+      }
     );
+      
   }
 }
