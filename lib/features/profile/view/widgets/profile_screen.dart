385bf8d1131fc177ec212956b7f60940dde8d7e0
qassem abied <qassemab1@gmail.com>
2026-02-15 02:40:05 +0200

    used riverpod replace cubit

--- lib/features/profile/view/widgets/profile_screen.dart
+++ lib/features/profile/view/widgets/profile_screen.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:untitled3/core/helpers/spacing.dart';
-import 'package:untitled3/features/profile/controller/profile_cubit.dart';
-import 'package:untitled3/features/profile/controller/profile_state.dart';
+
+import '../../controller/profile_riverpod.dart';
 
 String getCountryName(String code) {
   switch (code) {
@@ -26,83 +26,70 @@ String getLanguageName(String code) {
   }
 }
 
-class ProfileScreen extends StatelessWidget {
+class ProfileScreen extends ConsumerWidget {
   const ProfileScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, ref) {
+    final profile = ref.watch(profileProvider);
     return Scaffold(
       appBar: AppBar(title: const Text("Profile"), centerTitle: true),
-      body: BlocBuilder<ProfileCubit, ProfileState>(
-        builder: (context, state) {
-          return state.maybeWhen(
-            loading: () {
-              return const Center(child: CircularProgressIndicator());
-            },
-            error: (e) {
-              return Center(child: Text(e ?? ''));
-            },
-            success: (profile) {
-              return Padding(
-                padding: const EdgeInsets.symmetric(
-                  vertical: 20,
-                  horizontal: 16,
+      body: profile.when(
+        data: (prof) {
+          return Padding(
+            padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 16),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                CircleAvatar(
+                  radius: 60,
+                  backgroundImage: NetworkImage(
+                    prof.avatar != null
+                        ? "https://www.gravatar.com/avatar/${profile.hashCode}"
+                        : "https://i.imgur.com/BoN9kdC.png",
+                  ),
                 ),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    CircleAvatar(
-                      radius: 60,
-                      backgroundImage: NetworkImage(
-                        profile.avatar != null
-                            ? "https://www.gravatar.com/avatar/${profile.hashCode}"
-                            : "https://i.imgur.com/BoN9kdC.png",
-                      ),
-                    ),
 
-                    const SizedBox(height: 20),
+                const SizedBox(height: 20),
 
-                    Text(
-                      profile.username ?? '',
-                      style: const TextStyle(
-                        fontSize: 22,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-
-                    if (profile.name!.isNotEmpty)
-                      Text(
-                        profile.name ?? '',
-                        style: const TextStyle(fontSize: 16),
-                      ),
-
-                    const SizedBox(height: 20),
-
-                    Card(
-                      child: ListTile(
-                        leading: const Icon(Icons.language),
-                        title: const Text("Language"),
-                        subtitle: Text(getLanguageName(profile.iso6391 ?? '')),
-                      ),
-                    ),
-                    verticalSpace(20),
-                    Card(
-                      child: ListTile(
-                        leading: const Icon(Icons.flag),
-                        title: const Text("Country"),
-                        subtitle: Text(getCountryName(profile.iso31661 ?? '')),
-                      ),
-                    ),
-                  ],
+                Text(
+                  prof.username ?? '',
+                  style: const TextStyle(
+                    fontSize: 22,
+                    fontWeight: FontWeight.bold,
+                  ),
                 ),
-              );
-            },
 
-            orElse: () {
-              return const SizedBox.shrink();
-            },
+                if (prof.name!.isNotEmpty)
+                  Text(prof.name ?? '', style: const TextStyle(fontSize: 16)),
+
+                const SizedBox(height: 20),
+
+                Card(
+                  child: ListTile(
+                    leading: const Icon(Icons.language),
+                    title: const Text("Language"),
+                    subtitle: Text(getLanguageName(prof.iso6391 ?? '')),
+                  ),
+                ),
+                verticalSpace(20),
+                Card(
+                  child: ListTile(
+                    leading: const Icon(Icons.flag),
+                    title: const Text("Country"),
+                    subtitle: Text(getCountryName(prof.iso31661 ?? '')),
+                  ),
+                ),
+              ],
+            ),
           );
+        },
+        error: (e, _) {
+          return Center(child: Text(e.toString() ?? ''));
+        },
+        loading: () {
+          return const Center(child: CircularProgressIndicator());
         },
       ),
     );
