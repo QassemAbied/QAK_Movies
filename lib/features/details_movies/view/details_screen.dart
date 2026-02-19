bd88ab6280af19f7e0f71dbd7c7d1991f1cc3108
qassem abied <qassemab1@gmail.com>
2026-02-19 05:00:26 +0200

    refactor code

--- lib/features/details_movies/view/details_screen.dart
+++ lib/features/details_movies/view/details_screen.dart
@@ -1,16 +1,22 @@
+import 'dart:developer';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:untitled3/core/helpers/extension.dart';
-import 'package:untitled3/features/details_movies/controller/details_movies_cubit.dart';
-import 'package:untitled3/features/details_movies/controller/details_movies_state.dart';
 import 'package:untitled3/features/details_movies/view/widgets/back_ground_image.dart';
 import 'package:untitled3/features/details_movies/view/widgets/content_details_view.dart';
 import 'package:untitled3/features/details_movies/view/widgets/shimmar_for_details_view.dart';
+import 'package:untitled3/features/providers.dart';
+
+import '../data/models/details_params.dart';
 
 class DetailsScreen extends StatelessWidget {
-  const DetailsScreen({super.key});
+ const  DetailsScreen({super.key, required this.movieId});
+  final int movieId;
+
   @override
   Widget build(BuildContext context) {
+    final params = DetailsParams(movieId: movieId, language: 'en');
+    log('messageBuild');
     return SafeArea(
       child: Scaffold(
         extendBodyBehindAppBar: true,
@@ -19,39 +25,44 @@ class DetailsScreen extends StatelessWidget {
           elevation: 3,
           iconTheme: IconThemeData(color:context.colors.onSurface),
         ),
-        body: BlocBuilder<DetailsMoviesCubit, DetailsMoviesState>(
-          builder: (context, state) {
-            return state.maybeWhen(
-              detailsLoading: () {
-                return const  DetailsShimmerLoading();
-              },
-              detailsError: (error) {
-                return Center(child: Text(error));
-              },
-              detailsSuccess: (detailsMovies, credits, images, similar, video) {
+        body:Consumer(builder: (BuildContext context, WidgetRef ref, Widget? child) {
+          final detailsRef= ref.watch(detailsProvider(params,));
+
+
+          return detailsRef.when(
+              data: (detailsData){
                 return Stack(
                   children: [
-                    BackGroundImage(detailsMovies: detailsMovies),
+                    BackGroundImage(detailsMovies: detailsData.details),
                     Container(
                       height: MediaQuery.of(context).size.height / 3,
                       color: Colors.transparent,
                     ),
                     ContentDetailsView(
-                      detailsMovies: detailsMovies,
-                      images: images,
-                      video: video,
-                      credits: credits,
-                      similar: similar,
+                      detailsMovies: detailsData.details,
+                      images: detailsData.backdrops,
+                      video: detailsData.videos,
+                      credits: detailsData.cast,
+                      similar: detailsData.similarMovies,
                     ),
                   ],
                 );
               },
-              orElse: () {
-                return const SizedBox();
+              error: (error,_) {
+                return Center(child: Text(error.toString()));
               },
-            );
-          },
+              loading: (){
+                return const  DetailsShimmerLoading();
+              }
+          );
+        },
+
         ),
+
+
+
+
+
       ),
     );
   }
