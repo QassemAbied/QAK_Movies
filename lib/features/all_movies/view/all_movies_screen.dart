308a9e2653d8a3165aa489cb8e160b9f1385086c
qassem abied <qassemab1@gmail.com>
2026-02-20 22:32:12 +0200

    refactor code

--- lib/features/all_movies/view/all_movies_screen.dart
+++ lib/features/all_movies/view/all_movies_screen.dart
@@ -1,52 +1,43 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:untitled3/core/theming/theme_cubit/app_theme_cubit.dart';
-import 'package:untitled3/features/all_movies/controller/all_movies_cubit.dart';
-import 'package:untitled3/features/all_movies/controller/all_movies_state.dart';
-import 'package:untitled3/features/all_movies/data/repos/all_movies_repos.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:untitled3/features/all_movies/data/models/all_movies_params.dart';
 import 'package:untitled3/core/common_widgets/list_view/all_movies_list_view.dart';
 import 'package:untitled3/core/common_widgets/list_view/all_movies_shimmer.dart';
 import 'package:untitled3/features/all_movies/view/widgets/app_bar_widget.dart';
 import 'package:untitled3/features/home/data/models/movies_category.dart';
-import '../../../core/di.dart';
+import 'package:untitled3/features/providers.dart';
 import '../../../core/helpers/extension.dart';
+import '../../../core/theming/settings_controller/settings_riverpod.dart';
 
-class AllMoviesScreen extends StatefulWidget {
+class AllMoviesScreen extends ConsumerStatefulWidget {
   final MovieCategory category;
 
   const AllMoviesScreen({super.key, required this.category});
 
   @override
-  State<AllMoviesScreen> createState() => _AllMoviesScreenState();
+  ConsumerState<AllMoviesScreen> createState() => _AllMoviesScreenState();
 }
 
-class _AllMoviesScreenState extends State<AllMoviesScreen> {
-  late AllMoviesCubit _cubit;
+class _AllMoviesScreenState extends ConsumerState<AllMoviesScreen> {
   late final ScrollController _scrollController;
+
+  late final lang = ref.watch(appSettingsProvider).locale;
+  late final params = AllMoviesParams(lang.code, widget.category);
 
   @override
   void initState() {
     super.initState();
-
-    _cubit = AllMoviesCubit(
-      allMoviesRepository: Sl<AllMoviesRepository>(),
-      category: widget.category,
-      language: AppThemeCubit.get(context).currentLanguage.code,
-    )..loadFirstPage();
-
     _scrollController = ScrollController()..addListener(_onScroll);
   }
 
   void _onScroll() {
     if (!_scrollController.hasClients) return;
-
     final max = _scrollController.position.maxScrollExtent;
     final current = _scrollController.position.pixels;
-
     final threshold = max - 300;
-
     if (current >= threshold) {
-      _cubit.loadMore();
+      ref.read(allMoviesProvider(params).notifier).loadMore();
     }
   }
 
@@ -58,39 +49,28 @@ class _AllMoviesScreenState extends State<AllMoviesScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return BlocProvider.value(
-      value: _cubit,
-      child: Scaffold(
-        appBar: AppBar(
-
-          title: AppBarWidget(category: widget.category),
-        ),
-        body: Padding(
-          padding: const EdgeInsets.symmetric(vertical: 18, horizontal: 18),
-          child: BlocBuilder<AllMoviesCubit, AllMoviesState>(
-            buildWhen: (previous, current) =>
-                current is Loading || current is Success || current is Error,
-            builder: (context, state) {
-              return state.maybeWhen(
-                loading: () {
-                  return const AllMoviesShimmerList();
-                },
-                success: (movieModel, hasMore) {
-                  return AllMoviesWidget(
-                    scrollController: _scrollController,
-                    movieModel: movieModel,
-                    hasMore: hasMore,
-                  );
-                },
-                error: (error) {
-                  return Center(child: Text(error.message ?? ''));
-                },
-                orElse: () {
-                  return const SizedBox();
-                },
-              );
-            },
-          ),
+    final allMovieState = ref.watch(allMoviesProvider(params));
+    return Scaffold(
+      appBar: AppBar(title: AppBarWidget(category: widget.category)),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 18, horizontal: 18),
+        child: allMovieState.when(
+          data: (movieModel) {
+            return AllMoviesWidget(
+              scrollController: _scrollController,
+              movieModel: movieModel.movies,
+              hasMore: movieModel.hasMore,
+            );
+          },
+          error: (error, _) {
+            return Center(child: Text(error.toString() ?? ''));
+            if (kDebugMode) {
+              print(error.toString());
+            }
+          },
+          loading: () {
+            return const AllMoviesShimmerList();
+          },
         ),
       ),
     );
